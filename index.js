26e6ac4a937dffd5cca23580152c5945184b11c6
Damien Wheatley <DamienWheatley@HF-T-16-030.harrisnet.internal>
2018-10-29 14:42:31 +0000

    Added the ability to choose a 'function'.

--- index.js
+++ index.js
@@ -6,24 +6,37 @@ console.log('========================');
 //input first number
 console.log('Please enter your first number');
 const firstresponse = readline.prompt();
-const firstnumber = parseInt(firstresponse,10);
+const firstnumber = parseFloat(firstresponse,10);
 
-/*specify what to do with the numbers?
-console.log('What would you like to do? Type add, minus, multiply, divide');
+//specify what to do?
+console.log('What would you like to do? Input: +, -, *, /');
 const action = readline.prompt();
-
-if (action=='add') {let ADD = '+'
-    
-    } else if (action=='minus') {let MINUS = '-'
-    } else if (action=='multiply') {
-    } else if (action=='divide') {let DIVIDE = '/'}
-*/
 
 //input second number
 console.log('Please enter your second number')
 const secondresponse = readline.prompt();
-const secondnumber = parseInt(secondresponse,10);
+const secondnumber = parseFloat(secondresponse,10);
+
+let answer;
+
+switch (action) {
+
+    case '+':
+        answer = firstnumber + secondnumber;
+        break;
+    
+    case '-':
+        answer = firstnumber - secondnumber;
+        break;
+
+    case '*':
+        answer = firstnumber * secondnumber;
+        break;
+    
+    case '/':
+        answer = firstnumber / secondnumber;
+        break;
+                }
 
 //display answer
-const answer = firstnumber + secondnumber;
 console.log('The answer is ' + answer);
